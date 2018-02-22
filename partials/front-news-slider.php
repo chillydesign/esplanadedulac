ce73795dfd263e692fddd8b027817a9dd118043e
Charles Harvey <harvey.charles@gmail.com>
2018-02-22 16:14:21 +0100

    styled for mobile home page; added variables for browser widths

---
+++ partials/front-news-slider.php
@@ -0,0 +1,17 @@
+
+<section id="news_slider">
+    <div class="container">
+        <div class="single_news_container">
+
+            <div class="news_image"></div>
+            <div class="news_text">
+                <p class="category">ACTUALITéS L’Esplanade DU LAC</p>
+                <h3>Fermeture de la billetterie pour les fêtes</h3>
+                <p class="date">Du 15 Décembre au 2 Janvier</p>
+                <p>La billetterie de L’Esplanade du lac est ouverte jusqu’au vendredi 22 décembre 18h. La billetterie ouvrira le jeudi 4 janvier 2018 à 14h. Pendant les vacances de noël, vous avez la possibilité d’acheter vos billets à Office de Tourisme Divonne-les-Bains (Ain).</p>
+                <p>Toute l'équipe vous souhaite de très joyeuses fêtes de fin d'année et se réjouit de vous retrouver à la rentrée pour poursuivre cette saison culturelle.</p>
+                <p><a href="#" class="button">Lire plus</a></p>
+            </div>
+        </div>
+    </div>
+</section>
