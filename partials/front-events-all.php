2e8f4308224d722c86f45e4bcb181936d631d757
Charles Harvey <harvey.charles@gmail.com>
2018-02-22 15:25:56 +0100

    added html and stlying for events on home page

--- partials/front-events-all.php
+++ partials/front-events-all.php
@@ -1,7 +1,34 @@
 
 <section id="all_events">
-<h2>Saison 2017 - 2018</h2>
+    <h2 id="all_events_title">Saison 2017 - 2018</h2>
+
+    <div class="container">
+        <div id="events_and_programme">
+
+            <div class="events_container">
+
+                <?php for ($i=0; $i < 9 ; $i++) { ?>
+
+                    <div class="single_event_container">
+                        <a  href="#" class="single_event_image"></a>
+                        <p class="category">Théâtre</p>
+                        <h4><a href="#">Apothéose du fait divers, c’est tout mais ça fait peur!</a></h4>
+                        <p class="date">JE 29 Mars - 20H30</p>
+                    </div>
 
 
+
+                <?php } ?>
+
+
+
+
+            </div>
+            <div class="programme_container">
+                    <h4>L’AGENDA</h4>
+
+            </div>
+        </div>
+    </div>
 
 </section>
