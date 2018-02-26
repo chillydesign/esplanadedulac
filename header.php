b51a4ccfb7c8662ff782e8ea69b029080b478b2c
Charles Harvey <harvey.charles@gmail.com>
2018-02-26 18:59:08 +0100

    add dosis google font link in header

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="<?php bloginfo('description'); ?>">
-        <!-- <link href="https://fonts.googleapis.com/css?family=Dosis:300,700" rel="stylesheet"> -->
+        <link href="https://fonts.googleapis.com/css?family=Dosis:300,700" rel="stylesheet">
         <?php wp_head(); ?>
 
 
