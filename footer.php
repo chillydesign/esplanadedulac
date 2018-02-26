078610e72c38b0106dc2bf8f9d4ed0a16f194aae
Charles Harvey <harvey.charles@gmail.com>
2018-02-26 11:01:30 +0100

    added form fields to footer;

--- footer.php
+++ footer.php
@@ -14,45 +14,54 @@
                 <div class="footer_column">
                     <h3>Suivez-nous</h3>
                     <p> <a  class="social_icon social_icon_facebook" href="#">Suivez-nous sur Facebook</a>  <br>
-                     <a  class="social_icon social_icon_instagram" href="#">Suivez-nous sur Instagram</a> </p>
+                        <a  class="social_icon social_icon_instagram" href="#">Suivez-nous sur Instagram</a> </p>
 
-                </div>
-                <div class="footer_column">
-                    <h3>Newsletter</h3>
+                    </div>
+                    <div class="footer_column footer_column_wide">
+                        <h3>Newsletter</h3>
+
+                        <form action="" >
+                            <input type="text" autocomplete="given-name"  id="first_name" name="first_name" placeholder="Prénom" />
+                            <input type="text" autocomplete="family-name" id="last_name" name="last_name" placeholder="Nom" />
+                            <input type="text"   autocomplete="email" id="email" name="email" placeholder="Adresse email" />
+                            <button type="submit" id="submit_newsletter">S'inscrire</button>
+                        </form>
+
+
+                    </div>
+
                 </div>
 
             </div>
 
-        </div>
+
+            <nav>
+                <div class="container">
+                    <ul>
+                        <?php chilly_nav('footer_nav'); ?>
+                        <li><a href="https://webfactor.ch">Website by Webfactor</a></li>
+                    </ul>
+                </div>
+            </nav>
+
+        </footer>
 
 
-        <nav>
-            <div class="container">
-                <ul>
-                    <?php chilly_nav('footer_nav'); ?>
-                    <li><a href="https://webfactor.ch">Website by Webfactor</a></li>
-                </ul>
-            </div>
-        </nav>
-
-    </footer>
-
-
-</main>
+    </main>
 
 
 
-<?php wp_footer(); ?>
-<script type="text/javascript" src="https://www.youtube.com/iframe_api"></script>
-<script type='text/javascript' src='//maps.google.com/maps/api/js?key=AIzaSyC-BDJZU14ltCrYRPei33a4ZSQfJqRbxNY&#038;ver=4.8.1'></script>
-<script type="text/javascript" src="<?php echo $tdu; ?>/js/min/scripts.bundle.js?v=<?php echo wf_version(); ?>"></script>
-<script>
-// (function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-// (f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-// l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-// })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-// ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-// ga('send', 'pageview');
+    <?php wp_footer(); ?>
+    <script type="text/javascript" src="https://www.youtube.com/iframe_api"></script>
+    <script type='text/javascript' src='//maps.google.com/maps/api/js?key=AIzaSyC-BDJZU14ltCrYRPei33a4ZSQfJqRbxNY&#038;ver=4.8.1'></script>
+    <script type="text/javascript" src="<?php echo $tdu; ?>/js/min/scripts.bundle.js?v=<?php echo wf_version(); ?>"></script>
+    <script>
+    // (function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
+    // (f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
+    // l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
+    // })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    // ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
+    // ga('send', 'pageview');
 </script>
 
 </body>
