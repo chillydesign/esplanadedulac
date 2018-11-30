8ce722435617fd40111504fe8eabb80da998ae1f
Charles Harvey <harvey.charles@gmail.com>
2018-11-30 10:55:15 +0100

    replaced google api key

--- footer.php
+++ footer.php
@@ -75,7 +75,7 @@
 
   <?php wp_footer(); ?>
   <script type="text/javascript" src="https://www.youtube.com/iframe_api"></script>
-  <script type='text/javascript' src='//maps.google.com/maps/api/js?key=AIzaSyC-BDJZU14ltCrYRPei33a4ZSQfJqRbxNY&#038;ver=4.8.1'></script>
+  <script type='text/javascript' src='//maps.google.com/maps/api/js?key=AIzaSyCoHn6xGs3f0pMSx7zh7D0M1w1K6iS51tc&#038;ver=4.8.1'></script>
   <script type="text/javascript" src="<?php echo $tdu; ?>/js/min/scripts.bundle.js?v=<?php echo wf_version(); ?>"></script>
   <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-114588276-4"></script>
