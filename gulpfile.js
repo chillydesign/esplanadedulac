8456a07c4c35919624d70dae4738d76caf27ae28
Charles Harvey <harvey.charles@gmail.com>
2019-06-19 14:06:31 +0100

    updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,46 +2,133 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var minify = require('gulp-minify');
+var watch = require('gulp-watch');
+var realFavicon = require ('gulp-real-favicon');
+var fs = require('fs');
+
+// File where the favicon markups are stored
+var FAVICON_DATA_FILE = 'faviconData.json';
+
 //var browserSync = require('browser-sync');
 
 
-gulp.task('heya', function() {
+gulp.task('heya', function(done) {
   console.log('I live! Gulp is alive!');
+      done();
 });
 
 
-gulp.task('sass', function(){
-  return gulp.src('scss/global.scss')
+gulp.task('sass', function(done){
+  return gulp.src('scss/style.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
     .pipe(autoprefixer({
-        cascade: false
+        cascade: false,
+        browsers: ['last 3 versions'],
     }))
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('css'))
+    .pipe(gulp.dest('css'));
+    done();
+
+});
+
+gulp.task('watch', function () {
+    watch('scss/*.scss',  gulp.series( 'sass' ));
 });
 
 
 
-
-gulp.task('compress', function() {
-  gulp.src('js/*.js')
-    .pipe(minify({
-        ext:{
-            src:'-debug.js',
-            min:'.js'
+// Generate the icons. This task takes a few seconds to complete.
+// You should run it at least once to create the icons. Then,
+// you should run it whenever RealFaviconGenerator updates its
+// package (see the check-for-favicon-update task below).
+gulp.task('generate-favicon', function(done) {
+  realFavicon.generateFavicon({
+    masterPicture: 'images/favicon.png',
+    dest: 'images/favicons',
+    iconsPath: '/',
+    design: {
+      ios: {
+        pictureAspect: 'noChange',
+        assets: {
+          ios6AndPriorIcons: false,
+          ios7AndLaterIcons: false,
+          precomposedIcons: false,
+          declareOnlyDefaultIcon: true
+        }
+      },
+      desktopBrowser: {},
+      windows: {
+        pictureAspect: 'noChange',
+        backgroundColor: '#235ba8',
+        onConflict: 'override',
+        assets: {
+          windows80Ie10Tile: false,
+          windows10Ie11EdgeTiles: {
+            small: false,
+            medium: true,
+            big: false,
+            rectangle: false
+          }
+        }
+      },
+      androidChrome: {
+        pictureAspect: 'noChange',
+        themeColor: '#235ba8',
+        manifest: {
+          display: 'standalone',
+          orientation: 'notSet',
+          onConflict: 'override',
+          declared: true
         },
-        exclude: ['tasks'],
-        ignoreFiles: ['.combo.js', '-min.js' , 'jquery.js' , '.min.js' ]
-    }))
-    .pipe(gulp.dest('js/min'))
+        assets: {
+          legacyIcon: false,
+          lowResolutionIcons: false
+        }
+      },
+      safariPinnedTab: {
+        pictureAspect: 'silhouette',
+        themeColor: '#235ba8'
+      }
+    },
+    settings: {
+      scalingAlgorithm: 'Mitchell',
+      errorOnImageTooSmall: false,
+      readmeFile: false,
+      htmlCodeFile: false,
+      usePathAsIs: false
+    },
+    markupFile: FAVICON_DATA_FILE
+  }, function() {
+    done();
+  });
 });
 
+// Inject the favicon markups in your HTML pages. You should run
+// this task whenever you modify a page. You can keep this task
+// as is or refactor your existing HTML pipeline.
+gulp.task('inject-favicon-markups', function() {
+  return gulp.src([ 'includes/header.php' ])
+    .pipe(realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code))
+    .pipe(gulp.dest('testfav'));
+});
 
+// Check for updates on RealFaviconGenerator (think: Apple has just
+// released a new Touch icon along with the latest version of iOS).
+// Run this task from time to time. Ideally, make it part of your
+// continuous integration system.
+gulp.task('check-for-favicon-update', function(done) {
+  var currentVersion = JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).version;
+  realFavicon.checkForUpdates(currentVersion, function(err) {
+    if (err) {
+      throw err;
+    }
+  });
+});
 
 
 
 // Run all Gulp tasks and serve application
-gulp.task('default', ['heya', 'sass'], function() {
-  gulp.watch('scss/**/*.scss', ['sass']);
-//  gulp.watch('js/**/*.js',  ['compress'] );
+gulp.task('default', gulp.series( 'sass', 'watch'), function() {
+    // sass function now incldes the watching of files
+
 });
