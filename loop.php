07416ad36311ee89bfbf802a21004ea9d3e56d55
Charles Harvey <harvey.charles@gmail.com>
2019-06-30 08:50:12 +0100

    fix styling on family image front page

--- loop.php
+++ loop.php
@@ -1,3 +1,5 @@
+<?php $family_image =  get_field('logo_voir_famille','option');   // returns url of the image ?>
+
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
 	<?php $event_id = get_the_id(); ?>
@@ -7,16 +9,20 @@
 	<?php $date = get_field('date');  ?>
 	<?php $nice_date =  utf8_encode(strftime("%a %d %B", strtotime( $date ))); ?>
 	<?php $time = get_field('time'); ?>
-	<?php $familyclass = (get_field('family')) ? 'family ' : '' ; ?>
+	<?php $family = get_field('family') ; ?>
 
 	<div class="single_event_container">
-		<div class="<?php echo $familyclass;?>single_event_inner">
+		<div class="single_event_inner">
 			<div class="single_event_image_container">
 				<a  href="<?php echo $permalink; ?>" class="single_event_image" style="background-image:url(<?php echo $image; ?>);"></a>
 			</div>
 			<?php if ($category AND sizeof($category) > 0) :?><p class="category"><?php echo $category[0]->name; ?></p><?php endif; ?>
 			<h4><a href="<?php echo $permalink; ?>"><?php the_title(); ?></a></h4>
 			<?php if ($date): ?><p class="date"> <?php echo $nice_date; ?>  <br> <?php echo $time; ?></p><?php endif; ?>
+
+			<?php if ($family) : ?>
+				<div class="voir_en_famille_inside" style="background-image: url(<?php echo $family_image; ?>);"></div>
+			<?php endif; ?>
 		</div>
 	</div>
 
