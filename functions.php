37f3a2e61ff79f14900e29a8d476df1c46b59576
Charles Harvey <harvey.charles@gmail.com>
2021-02-24 14:05:06 +0000

    adf

--- functions.php
+++ functions.php
@@ -685,7 +685,8 @@ function generate_color_stylesheet($color) {
     html body #latest_events_slider .single_event_slide_inner { background-color: ' . $color . '  }
     html body .accordion_container .single_event_container:after { background-color: ' . $color . '  }
     html body .accordion_container .single_accordion  h3.accordion_title {color: ' . $color . '  }
-    html body h5, .page h3  {color: ' . $color . '  }
+    html body h5, 
+    html body .page h3  {color: ' . $color . '  }
     html body a:hover {color: ' . $color . '  }
     html body header#page_header  nav ul li.menu-item-has-children ul.sub-menu  {border-top-color: ' . $color . '}
     html body div.response { border-color: ' . $color . '  }
