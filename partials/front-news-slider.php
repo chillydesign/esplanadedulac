3bf0dd07661af3cf277a887098811cd9e6a980a1
Charles Harvey <harvey.charles@gmail.com>
2021-04-21 11:08:26 +0100

    change news image to full width

--- partials/front-news-slider.php
+++ partials/front-news-slider.php
@@ -1,30 +1,31 @@
-
 <section id="news_slider">
-    <div class="container">
-        <div class="carousel">
-        <?php $latest_news = new WP_Query(array('post_type' => 'post',   'posts_per_page' =>  3 ));  ?>
-        <?php  if ($latest_news->have_posts() ) :  while($latest_news->have_posts()) : $latest_news->the_post();  ?>
-            <?php $permalink = get_the_permalink(); ?>
-            <?php $post_id = get_the_id(); ?>
-            <?php $image = thumbnail_of_post_url( $post_id,  'large');  ?>
-            <div class="single_news_container">
+  <h3 class="news_title">Actualités</h3>
+  <div class="container">
+    <div class="carousel">
+      <?php $latest_news = new WP_Query(array('post_type' => 'post',   'posts_per_page' =>  3));  ?>
+      <?php if ($latest_news->have_posts()) :  while ($latest_news->have_posts()) : $latest_news->the_post();  ?>
+          <?php $permalink = get_the_permalink(); ?>
+          <?php $post_id = get_the_id(); ?>
+          <?php $image = thumbnail_of_post_url($post_id,  'large');  ?>
+          <div class="single_news_container">
             <div class="single_news_container_inner">
-              <?php if(get_field('video')) : ?>
+              <?php if (get_field('video')) : ?>
                 <div class="news_image"><?php the_field('video'); ?></div>
-              <?php else: ?>
+              <?php else : ?>
                 <div class="news_image" style="background-image:url(<?php echo $image; ?>);"></div>
               <?php endif; ?>
-                <div class="news_text">
-                    <p class="category">Actualités</p>
-                    <h3><a href="<?php echo $permalink; ?>"><?php the_title(); ?></a></h3>
-                    <!-- <p class="date"><?php the_time('F j, Y'); ?></p> -->
-                    <p><?php the_excerpt(); ?></p>
-                </div>
-            </div>
-            </div>
+              <div class="news_text">
 
-        <?php endwhile;endif;  ?>
-        <?php wp_reset_query(); ?>
+                <h3><a href="<?php echo $permalink; ?>"><?php the_title(); ?></a></h3>
+                <!-- <p class="date"><?php the_time('F j, Y'); ?></p> -->
+                <p><?php the_excerpt(); ?></p>
+              </div>
+            </div>
+          </div>
+
+      <?php endwhile;
+      endif;  ?>
+      <?php wp_reset_query(); ?>
     </div>
-    </div>
+  </div>
 </section>
