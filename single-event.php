40869ddf7093bb951ab07f30942eebf75f688ca5
Melissa Rissel <rissel.melissa@gmail.com>
2021-07-29 15:04:46 +0100

    nah

--- single-event.php
+++ single-event.php
@@ -243,6 +243,13 @@
             <div><?php echo $tarifs; ?></div>
           <?php endif; ?>
 
+          <?php if ($voltaire) : ?>
+            <?php $vol_url = get_field('saison_voltaire_url', 'option'); ?>
+            <?php $vol_img = get_field('saison_voltaire_image', 'option'); ?>
+            <div class="event_special_image_container">
+              <a href="<?php echo $vol_url; ?>" target="_blank"><img src="<?php echo $vol_img; ?>"><span>Saison Voltaire</span></a>
+            </div>
+          <?php endif; ?>
           <?php if ($passedanse) : ?>
             <?php $pas_url = get_field('passe_danse_url', 'option'); ?>
             <?php $pas_img = get_field('passe_danse_image', 'option'); ?>
@@ -256,14 +263,6 @@
             <?php $cul_img = get_field('culture_pour_tous_image', 'option'); ?>
             <div class="event_special_image_container">
               <a href="<?php echo $cul_url; ?>" target="_blank"><img src="<?php echo $cul_img; ?>"><span>Culture Pour Tous</span></a>
-            </div>
-          <?php endif; ?>
-
-          <?php if ($voltaire) : ?>
-            <?php $vol_url = get_field('saison_voltaire_url', 'option'); ?>
-            <?php $vol_img = get_field('saison_voltaire_image', 'option'); ?>
-            <div class="event_special_image_container">
-              <a href="<?php echo $vol_url; ?>" target="_blank"><img src="<?php echo $vol_img; ?>"><span>Saison Voltaire</span></a>
             </div>
           <?php endif; ?>
 
